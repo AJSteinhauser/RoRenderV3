f269735215d5adf083ab609194b39368e63cc6c0
AJ Steinhauser <ajsteinhauser11@gmail.com>
2023-07-02 19:49:55 -0400

    Minor fixes for deployment

--- imageRender.js
+++ imageRender.js
@@ -17,94 +17,91 @@ let tmpdir = null;
 
 // Stores the render data for the current render.
 let render = {
-    imageSize: { x: 0, y: 0 },
-    numPixels: 0,
+  imageSize: { x: 0, y: 0 },
+  numPixels: 0,
 
-    pixelData: new Uint8Array(),
-    nextIndex: 0,
+  pixelData: new Uint8Array(),
+  nextIndex: 0,
+};
+
+function exportimage(path) {
+  const image = sharp(render.pixelData, {
+    raw: {
+      width: render.imageSize.x,
+      height: render.imageSize.y,
+      channels: 4,
+    },
+  });
+  image.toFile(path);
 }
 
-function exportimage(path){
-	const image = sharp(render.pixelData, {
-        raw: {
-            width: render.imageSize.x,
-            height: render.imageSize.y,
-            channels: 4,
-        }
-    });
-	image.toFile(path)
+function getCompleationPercent() {
+  return Math.ceil(percentComplete);
 }
 
-function getCompleationPercent(){
-	return Math.ceil(percentComplete);
-}
-
-function getLatestImage(){
-	return latestImage; 
+function getLatestImage() {
+  return latestImage;
 }
 
 // Starts the server.
 function start(tmp) {
-	tmpdir = tmp;
-	percentComplete = 0;
-	fastify.listen({ port: 8081 }, (err, address) => {
-		if (err) {
-			console.error(err);
-			process.exit();
-		}
-		console.log(`Server is now listening on address: ${address}`);
-		
-	})
+  tmpdir = tmp;
+  percentComplete = 0;
+  fastify.listen({ port: 8081 }, (err, address) => {
+    if (err) {
+      console.error(err);
+      process.exit();
+    }
+    console.log(`Server is now listening on address: ${address}`);
+  });
 
-	// Get request for if the user opens the url in their web browser.
-	fastify.get("/", (request, reply) => {
-		reply.send("RoRenderV3 server is running.");
-	})
+  // Get request for if the user opens the url in their web browser.
+  fastify.get("/", (request, reply) => {
+    reply.send("RoRenderV3 server is running.");
+  });
 
-	// Resets the render data and sets it to the new image size.
-	fastify.post("/render-begin", (request, reply) => {
-		console.log("Render begin:");
-	
-		render.imageSize = request.body.imageSize;
-		render.numPixels = (render.imageSize.x * render.imageSize.y);
-		render.pixelData = new Uint8Array(render.numPixels * 4);
-		render.nextIndex = 0;
-	
-		reply.send();
-	})
+  // Resets the render data and sets it to the new image size.
+  fastify.post("/render-begin", (request, reply) => {
+    console.log("Render begin:");
 
-	// Stores the received pixel data in the render data.
-	fastify.post("/data", (request, reply) => {
-		for (let pixelComponent of request.body)
-			render.pixelData[render.nextIndex++] = pixelComponent;
-		
-		percentComplete = ((((render.nextIndex - 1) / 4) / render.numPixels) * 100);
+    render.imageSize = request.body.imageSize;
+    render.numPixels = render.imageSize.x * render.imageSize.y;
+    render.pixelData = new Uint8Array(render.numPixels * 4);
+    render.nextIndex = 0;
 
-		if (Date.now() - lastRenderTime > 1000){
-			
-			lastRenderTime = Date.now();
-			const image = sharp(render.pixelData, {
-				raw: {
-					width: render.imageSize.x,
-					height: render.imageSize.y,
-					channels: 4,
-				}
-			});
-			image.toFile(tmpdir.concat("/img",".png")).then(
-				() => latestImage *= -1
+    reply.send();
+  });
 
-			);
-		}
-		reply.send();
-	})
+  // Stores the received pixel data in the render data.
+  fastify.post("/data", (request, reply) => {
+    for (let pixelComponent of request.body)
+      render.pixelData[render.nextIndex++] = pixelComponent;
 
-	// Uses Sharp to create a png image of the render, then clears the pixel data from memory.
-	fastify.post("/render-done", (request, reply) => {
-		console.log("Render done.");
-		console.log("Generating image...");
-		reply.send();
-	})
+    percentComplete = ((render.nextIndex - 1) / 4 / render.numPixels) * 100;
+    console.log(render.numPixels);
+
+    if (Date.now() - lastRenderTime > 1000) {
+      lastRenderTime = Date.now();
+      const image = sharp(render.pixelData, {
+        raw: {
+          width: render.imageSize.x,
+          height: render.imageSize.y,
+          channels: 4,
+        },
+      });
+      image
+        .toFile(tmpdir.concat("/img", ".png"))
+        .then(() => (latestImage *= -1));
+    }
+    reply.send();
+  });
+
+  // Uses Sharp to create a png image of the render, then clears the pixel data from memory.
+  fastify.post("/render-done", (request, reply) => {
+    console.log("Render done.");
+    console.log("Generating image...");
+    reply.send();
+  });
 }
 
-
-module.exports = {start,exportimage,getCompleationPercent,getLatestImage};
+module.exports = { start, exportimage, getCompleationPercent, getLatestImage };
--- main.js
+++ main.js
@@ -1,17 +1,13 @@
-
-
-const electron = require('electron');
-const url = require('url');
-const path = require('path');
+const electron = require("electron");
+const url = require("url");
+const path = require("path");
 const render = require("./imageRender.js");
 const fs = require("fs");
-const tmp = require('tmp');
-const unhandled = require('electron-unhandled');
+const tmp = require("tmp");
+const unhandled = require("electron-unhandled");
 
-
-
-const {app, BrowserWindow, Menu, ipcMain, dialog, ipcRenderer, shell} = electron;
-
+const { app, BrowserWindow, Menu, ipcMain, dialog, ipcRenderer, shell } =
+  electron;
 
 let mainWindow;
 let helpWindow;
@@ -21,108 +17,88 @@ unhandled();
 Menu.setApplicationMenu(false);
 
 const tmpdir = tmp.dirSync();
-console.log('Dir: ', tmpdir.name);
+console.log("Dir: ", tmpdir.name);
 tmp.setGracefulCleanup();
 
+app.on("ready", function () {
+  mainWindow = new BrowserWindow({
+    webPreferences: {
+      nodeIntegration: true,
+    },
+    show: false,
+    resizable: false,
+    height: 540,
+    width: 768,
+  });
 
-app.on("ready",function(){
-    var isWin = process.platform === "win32";
-    if (!isWin){
-        mainWindow = new BrowserWindow({
-            webPreferences: {
-                nodeIntegration: true
-            },
-            show: false,
-            resizable: false,
-            height: 540,
-            width: 768,
-            
+  mainWindow.once("ready-to-show", () => {
+    mainWindow.show();
+  });
 
-        });
+  mainWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "index.html"),
+      protocol: "file:",
+      slashes: true,
+    })
+  );
+
+  //Quit all windows and
+  mainWindow.on("closed", function () {
+    app.quit();
+  });
+
+  mainWindow.on("focus", function () {
+    if (helpWindow != null) {
+      helpWindow.close();
+      helpWindow = null;
     }
-    else{
-        mainWindow = new BrowserWindow({
-            webPreferences: {
-                nodeIntegration: true
-            },
-            show: false,
-            resizable: false,
-            height: 550,
-            width: 780,
-        }); 
-    }
-
-    mainWindow.once("ready-to-show", () =>{
-        mainWindow.show()
-    });
-
-    mainWindow.loadURL(url.format({
-        pathname: path.join(__dirname, "index.html"),
-        protocol: "file:",
-        slashes: true,    
-    }));
-
-    //Quit all windows and
-    mainWindow.on("closed", function(){
-        app.quit();
-    });
-
-    mainWindow.on("focus", function(){
-        if (helpWindow != null){
-            helpWindow.close();
-            helpWindow = null;
-        }
-    });
-
+  });
 });
 
-
 //Catch help button click
-ipcMain.on("helpClick",function(e,item){
-    var arg = "secondparam";
-    shell.openExternal("https://devforum.roblox.com/t/rorender-minimap-creator/963288")
+ipcMain.on("helpClick", function (e, item) {
+  var arg = "secondparam";
+  shell.openExternal(
+    "https://devforum.roblox.com/t/rorender-minimap-creator/963288"
+  );
 });
 
 //Catch start server click
-ipcMain.on("startServer",function(e,item){
-    render.start(tmpdir.name);
-    var check = function(){
-        var percent = render.getCompleationPercent();
-        if (percent > 0){
-            mainWindow.webContents.send("startTimer", "arg");
-            mainWindow.webContents.send("updatePercent", percent);
-            try {
-                let imagenum = render.getLatestImage().toString();
-                var path = tmpdir.name.concat("/img",".png");
-                if (fs.existsSync(path)) {
-                    mainWindow.webContents.send("updateImage",path);
-                }
-            } 
-            catch(err) {
-               console.log("/img" + render.getLatestImage().toString() + ".png");
-               console.log("Does not exist");
-            }
+ipcMain.on("startServer", function (e, item) {
+  render.start(tmpdir.name);
+  var check = function () {
+    var percent = render.getCompleationPercent();
+    if (percent > 0) {
+      mainWindow.webContents.send("startTimer", "arg");
+      mainWindow.webContents.send("updatePercent", percent);
+      try {
+        let imagenum = render.getLatestImage().toString();
+        var path = tmpdir.name.concat("/img", ".png");
+        if (fs.existsSync(path)) {
+          mainWindow.webContents.send("updateImage", path);
         }
-        if(percent == 100){
-            return true;
-        }
-        else {
-            setTimeout(check, 500); // check again in a second
-        }
+      } catch (err) {
+        console.log("/img" + render.getLatestImage().toString() + ".png");
+        console.log("Does not exist");
+      }
     }
-    check();
+    if (percent == 100) {
+      return true;
+    } else {
+      setTimeout(check, 500); // check again in a second
+    }
+  };
+  check();
 });
 
 //Catch export image click
-ipcMain.on("exportImage",function(e,item){
-    render.exportimage(
-        dialog.showSaveDialogSync(mainWindow,{
-            title: "Save image",
-            buttonLabel: "Save",
-            filters: [
-                {name: "png", extensions: ["png"]}
-            ]
-        })
-    );
+ipcMain.on("exportImage", function (e, item) {
+  render.exportimage(
+    dialog.showSaveDialogSync(mainWindow, {
+      title: "Save image",
+      buttonLabel: "Save",
+      filters: [{ name: "png", extensions: ["png"] }],
+    })
+  );
 });
-
