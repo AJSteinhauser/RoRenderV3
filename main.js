1ff2257773900d8811d175a9de3947dd5e21f451
AJ Steinhauser <ajsteinhauser11@gmail.com>
2023-10-17 23:30:00 -0400

    Working version

--- main.js
+++ main.js
@@ -1,15 +1,11 @@
 const electron = require("electron");
-const sharp = require("sharp");
 const url = require("url");
 const path = require("path");
-const render = require("./imageRender.js");
 const fs = require("fs");
-const tmp = require("tmp");
 const unhandled = require("electron-unhandled");
 const fastify = require("fastify")()
 
-const { app, BrowserWindow, Menu, ipcMain, dialog, ipcRenderer, shell } =
-  electron;
+const { app, BrowserWindow, Menu, ipcMain, dialog, ipcRenderer, shell } = electron;
 
 let mainWindow;
 let helpWindow;
@@ -19,92 +15,85 @@ let imageSize = {}
 
 Menu.setApplicationMenu(false);
 
-const tmpdir = tmp.dirSync();
-console.log("Dir: ", tmpdir.name);
-tmp.setGracefulCleanup();
-
 app.on("ready", function () {
-  mainWindow = new BrowserWindow({
-    webPreferences: {
-      nodeIntegration: true,
-    },
-    show: false,
-    resizable: false,
-    height: 540,
-    width: 768,
-  });
+    mainWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true,
+        },
+        show: false,
+        resizable: false,
+        height: 540,
+        width: 768,
+    });
 
-  mainWindow.once("ready-to-show", () => {
-    mainWindow.show();
-      mainWindow.webContents.openDevTools()
-  });
+    mainWindow.once("ready-to-show", () => {
+        mainWindow.show();
+    });
 
-  mainWindow.loadURL(
-    url.format({
-      pathname: path.join(__dirname, "index.html"),
-      protocol: "file:",
-      slashes: true,
-    })
-  );
+    mainWindow.loadURL(
+        url.format({
+            pathname: path.join(__dirname, "index.html"),
+            protocol: "file:",
+            slashes: true,
+        })
+    );
 
-  //Quit all windows and
-  mainWindow.on("closed", function () {
-    app.quit();
-  });
+    //Quit all windows and
+    mainWindow.on("closed", function () {
+        app.quit();
+    });
 
-  mainWindow.on("focus", function () {
-    if (helpWindow != null) {
-      helpWindow.close();
-      helpWindow = null;
-    }
-  });
+    mainWindow.on("focus", function () {
+        if (helpWindow != null) {
+            helpWindow.close();
+            helpWindow = null;
+        }
+    });
 });
 
 //Catch help button click
 ipcMain.on("helpClick", function (e, item) {
-  var arg = "secondparam";
-  shell.openExternal(
-    "https://devforum.roblox.com/t/rorender-minimap-creator/963288"
-  );
+    var arg = "secondparam";
+    shell.openExternal(
+        "https://devforum.roblox.com/t/rorender-minimap-creator/963288"
+    );
 });
 
 //Catch start server click
 ipcMain.on("startServer", function (e, item) {
     console.log("server started")
-  fastify.listen({ port: 8081 }, (err, address) => {
-    if (err) {
-      console.error(err);
-      process.exit();
-    }
-    console.log(`Server is now listening on address: ${address}`);
-  });
+    fastify.listen({ port: 8081 }, (err, address) => {
+        if (err) {
+            console.error(err);
+            process.exit();
+        }
+        console.log(`Server is now listening on address: ${address}`);
+    });
 
-  // Get request for if the user opens the url in their web browser.
-  fastify.get("/", (request, reply) => {
-    reply.send("RoRenderV3 server is running.");
-  });
+    // Get request for if the user opens the url in their web browser.
+        fastify.get("/", (request, reply) => {
+            reply.send("RoRenderV3 server is running.");
+        });
 
-  // Resets the render data and sets it to the new image size.
-  fastify.post("/render-begin", (request, reply) => {
-      console.log("start")
-    imageSize = request.body.imageSize;
-    mainWindow.webContents.send("render-begin", imageSize);
-    reply.send();
-  });
+    // Resets the render data and sets it to the new image size.
+        fastify.post("/render-begin", (request, reply) => {
+            console.log("start")
+            imageSize = request.body.imageSize;
+            mainWindow.webContents.send("render-begin", imageSize);
+            reply.send();
+        });
 
-  // Stores the received pixel data in the render data.
-  fastify.post("/data", (request, reply) => {
+    // Stores the received pixel data in the render data.
+        fastify.post("/data", (request, reply) => {
 
-      console.log("data")
-    mainWindow.webContents.send("data", request.body);
-    reply.send();
-  });
+            console.log("data")
+            mainWindow.webContents.send("data", request.body);
+            reply.send();
+        });
 
-  // Uses Sharp to create a png image of the render, then clears the pixel data from memory.
-  fastify.post("/render-done", (request, reply) => {
-      console.log("Done")
-    reply.send();
-  });
+        fastify.post("/render-done", (request, reply) => {
+            reply.send();
+        });
 });
 
 //Catch export image click
@@ -118,5 +107,4 @@ ipcMain.on("exportImage", function (e, dataURL) {
         Buffer.from(dataURL.split(",")[1], 'base64'),
         () => {console.log("done")}
     )
-
 });
